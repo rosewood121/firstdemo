0343dee44a2531e7ea0d508000db1d08dfab6a3e
rosewood121 <43944100+rosewood121@users.noreply.github.com>
2019-04-29 15:22:13 +0800

    Update test.java

--- src/githubtest/test.java
+++ src/githubtest/test.java
@@ -2,6 +2,11 @@ package githubtest;
 
 public class test {
 	public static void main(String[] args) {
-		System.out.println("githubÌ«ºÃÁË£¡£¡");
+		System.out.println("github1");
+		System.out.println("github2");
+		System.out.println("github3");
+		
+		
+		
 	}
 }
