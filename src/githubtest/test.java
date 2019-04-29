7aa2588384f080cd4669dd0f2ed6906f6b5e5a62
hjs <hjs@hjs-PC>
2019-04-29 16:16:23 +0800

    this is a update

--- src/githubtest/test.java
+++ src/githubtest/test.java
@@ -3,8 +3,9 @@ package githubtest;
 public class test {
 	public static void main(String[] args) {
 		System.out.println("github1");
-		System.out.println("github2");
-		System.out.println("github3");
+		for (int i = 0; i < 10; i++) {
+			System.out.println("github");
+		}
 		
 		
 		
